8598e6d06085547ef6d043d3a6778f86b4e6ef26
Toshisawa Daitsuku <65522118+Daitsuku@users.noreply.github.com>
2023-01-10 19:37:53 +0700

    update css&login

--- Project/login.php
+++ Project/login.php
@@ -9,7 +9,7 @@ session_start(); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="./css/main.css">
     
     <title>Login</title>
 </head>
